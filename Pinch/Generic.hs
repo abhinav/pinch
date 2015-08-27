ebc44e6979b7966ad8d5d2b606c1a35498899d3a
Abhinav Gupta <mail@abhinavg.net>
2015-08-26 23:56:26 -0700

    Revert "Pinchable Enumeration/Field don't need to be inside K1."
    This reverts commit 76b49e792b0a7b0818ae4282bc84426b50e07cd9.
    
    Without the wrapper, things like
    
        newtype Foo = Foo (Map Text Text)

--- Pinch/Generic.hs
+++ Pinch/Generic.hs
@@ -90,12 +90,6 @@ instance
     gPinch (R1 b) = gPinch b
     gUnpinch m = L1 <$> gUnpinch m <|> R1 <$> gUnpinch m
 
-
-instance Pinchable a => GPinchable (K1 i a) where
-    type GTag (K1 i a) = Tag a
-    gPinch (K1 a) = pinch a
-    gUnpinch = fmap K1 . unpinch
-
 ------------------------------------------------------------------------------
 
 -- | Fields of data types that represent structs, unions, and exceptions
@@ -137,28 +131,27 @@ field f (Field a) = Field <$> f a
 
 instance
   {-# OVERLAPPABLE #-} (Pinchable a, KnownNat n)
-  => Pinchable (Field n a) where
-    type Tag (Field n a) = TStruct
-
-    pinch (Field a) = struct [n .= a]
+  => GPinchable (K1 i (Field n a)) where
+    type GTag (K1 i (Field n a)) = TStruct
+    gPinch (K1 (Field a)) = struct [n .= a]
       where
         n = fromIntegral $ natVal (Proxy :: Proxy n)
 
-    unpinch m = Field <$> m .: n
+    gUnpinch m = K1 . Field <$> m .: n
       where
         n = fromIntegral $ natVal (Proxy :: Proxy n)
 
-instance (Pinchable a, KnownNat n) => Pinchable (Field n (Maybe a)) where
-    type Tag (Field n (Maybe a)) = TStruct
-
-    pinch (Field a) = struct [n ?= a]
+instance
+  (Pinchable a, KnownNat n)
+  => GPinchable (K1 i (Field n (Maybe a))) where
+    type GTag (K1 i (Field n (Maybe a))) = TStruct
+    gPinch (K1 (Field a)) = struct [n ?= a]
       where
         n = fromIntegral $ natVal (Proxy :: Proxy n)
 
-    unpinch m = Field <$> m .:? n
+    gUnpinch m = K1 . Field <$> m .:? n
       where
         n = fromIntegral $ natVal (Proxy :: Proxy n)
-
 
 ------------------------------------------------------------------------------
 
@@ -179,14 +172,12 @@ data Enumeration (n :: Nat) = Enumeration
 enum :: Enumeration n
 enum = Enumeration
 
-instance KnownNat n => Pinchable (Enumeration n) where
-    type Tag (Enumeration n) = TEnum
-
-    pinch Enumeration = VInt32 . fromIntegral $ natVal (Proxy :: Proxy n)
-
-    unpinch (VInt32 i)
-        | i == val  = return Enumeration
+instance KnownNat n => GPinchable (K1 i (Enumeration n)) where
+    type GTag (K1 i (Enumeration n)) = TEnum
+    gPinch (K1 Enumeration) = VInt32 . fromIntegral $ natVal (Proxy :: Proxy n)
+    gUnpinch (VInt32 i)
+        | i == val  = return (K1 Enumeration)
         | otherwise = Left $ "Couldn't match enum value " ++ show i
       where
         val = fromIntegral $ natVal (Proxy :: Proxy n)
-    unpinch x = Left $ "Failed to read enum. Got " ++ show x
+    gUnpinch x = Left $ "Failed to read enum. Got " ++ show x
