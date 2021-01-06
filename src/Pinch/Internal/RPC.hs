6028b693087ad1dd0dfd169fdccc21e9c67092b5
Philipp Hausmann <philipp.hausmann@tiko.energy>
2021-01-06 11:52:08 +0100

    Add way to extract result/exceptions from thrift calls more easily

--- src/Pinch/Internal/RPC.hs
+++ src/Pinch/Internal/RPC.hs
@@ -9,13 +9,19 @@ module Pinch.Internal.RPC
   , writeMessage
 
   , ReadResult(..)
+
+  , ThriftResult(..)
   ) where
 
 import           Pinch.Internal.Message
-import           Pinch.Protocol       (Protocol, deserializeMessage', serializeMessage)
-import           Pinch.Transport      (Connection, Transport, ReadResult(..))
+import           Pinch.Internal.Pinchable (Pinchable, Tag)
+import           Pinch.Internal.TType     (TStruct)
+import           Pinch.Protocol           (Protocol, deserializeMessage',
+                                           serializeMessage)
+import           Pinch.Transport          (Connection, ReadResult (..),
+                                           Transport)
 
-import qualified Pinch.Transport      as Transport
+import qualified Pinch.Transport          as Transport
 
 -- | A bi-directional channel to read/write Thrift messages.
 data Channel = Channel
@@ -40,3 +46,12 @@ readMessage chan = Transport.readMessage (cTransportIn chan) $ deserializeMessag
 
 writeMessage :: Channel -> Message -> IO ()
 writeMessage chan msg = Transport.writeMessage (cTransportOut chan) $ serializeMessage (cProtocolOut chan) msg
+
+-- | The Result datatype for a Thrift Service Method.
+class (Pinchable a, Tag a ~ TStruct) => ThriftResult a where
+  -- | The Haskell type returned when the Thrift call succeeds.
+  type ResultType a
+  -- | Tries to extract the result from a Thrift call. If the call threw any
+  -- of the Thrift exceptions declared for this Thrift service method,
+  -- the corresponding Haskell excpetions is thrown using `throwIO`.
+  unwrap :: a -> IO (ResultType a)
